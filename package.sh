6071bcd3931e78081a55d28c66ea8643e92f16af
nesti99 <n99.andrea@gmail.com>
2025-12-21 01:17:09 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,2 +1,11 @@
-apt update && apt upgrade && apt install -y python3 python3-pip ffmpeg v4l-utils
-pip install flask
+apt update && apt upgrade && apt install ffmpeg python3 python3-pip v4l-utils intel-media-va-driver
+wget https://www.magewell.com/files/drivers/ProCaptureForLinux_1.3.4418.tar.gz
+tar xzvf ProCaptureForLinux_1.3.4418.tar.gz
+cd ProCaptureForLinux_1.3.4418
+./install.sh
+cd /
+gh repo clone nesti99/v4l2-rtsp-bridge
+tar zxvf rtsp.tgz -C /
+systemctl daemon-reload
+systemctl enable rtsp-streamer
+systemctl start rtsp-streamer
